34ba74be64b74a7642b974c222d467d760996a49
hasantezcan <hasantezcan77@gmail.com>
2020-04-22 13:52:53 +0300

    Add axios package

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
-import React from "react";
+import React from 'react';
+import axios from 'axios';
+
+import './App.css'
 
 class App extends React.Component {   
+    state = { advice: '' }
+    
+    componentDidMount() {
+        console.log('COMPONENT DID MOUNT');
+        
+    }
+
     render() {
         return(
             <h1>APP</h1>
         );
     }
 }
+
+export default App;
